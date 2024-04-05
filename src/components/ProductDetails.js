1e8aaae9f604fe676abe83bbe51f36441f9b8149
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-04-05 23:21:33 +1100

    add ProductDetail component

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
 function ProductDetails () {
+    const [displayToy, setDisplayToy] = useState('');
+    const { id } = useParams()
 
-
+    useEffect(() => {
+        fetch(`http://localhost:8000/toys/${id}`)
+        .then(res => res.json())
+        .then(data => setDisplayToy(data))
+    },[id])
+    return(
+        <div className="display-toy">
+            <div className="display-image">
+                <img src={displayToy.image} alt="toy-image" />
+            </div>
+            <div className="toy-infomation">
+                <h2>{displayToy.name}</h2>
+                <p>{displayToy.price}</p>
+                {displayToy.clearance ? <p>Save {displayToy.discount}</p> : null }
+            </div>
+        </div>
+    )
 }
 
 export default ProductDetails;
