19d222de58d566e02fc18d2fc9fa0b14e6f39bed
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-04-07 00:06:09 +1100

    add search result list component

---
+++ src/components/SearchResultList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ProductItem from "./ProductItem";
+import "./SearchResultList.css";
+
+function SearchResultList({ searchResult }){
+    return (
+        <div className="search-result-list">
+            {searchResult.map((searchToy) => (
+                <div className="display-searchToy">
+                    <ProductItem 
+                            id={searchToy.id}
+                            key={searchToy.id}
+                            image={searchToy.image}
+                            name={searchToy.name}
+                            price={searchToy.price}
+                            clearance={ searchToy.clearance ? <p>Save {searchToy.discount}</p> : null } />
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default SearchResultList;
