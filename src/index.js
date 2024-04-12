e7ee1448215d45849968f18f7aa6964ded8ddc1c
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-04-12 16:58:13 +1000

    wraping context to app component

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { CartProvider } from './context/CartContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <CartProvider>
+    <BrowserRouter>
+        <App />    
+    </BrowserRouter>
+  </CartProvider>
 );
  
 // If you want to start measuring performance in your app, pass a function
