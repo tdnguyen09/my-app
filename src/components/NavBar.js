e51c9a103cad0cb5de82c0afa815aa4d4078060e
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-04-12 16:56:37 +1000

    adding link to logout component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,26 @@
 import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { CartContext } from "../context/CartContext";
+import { FaUser } from "react-icons/fa6";
 
 function NavBar(){
     const cart = useContext(CartContext);
     
+    
+    
     return (
         <div className="navbar">
-            <NavLink to="/">ATOK TOY SHOP</NavLink>
-            <NavLink to="/products">Toys</NavLink>
-            <NavLink to="/login">Login</NavLink>
-            <NavLink to="/signup">Sign up</NavLink>
-            <NavLink to="/cart">
+            <NavLink className="no-underline" to="/">ATOK TOY SHOP</NavLink>
+            <NavLink className="no-underline" to="/products">Toys</NavLink>
+            { cart.isLogin ? 
+            <NavLink className="no-underline"to="/logout" ><FaUser /></NavLink>
+            : <NavLink className="no-underline"to="/login"><FaUser /></NavLink>
+            }
+            <NavLink className="no-underline"to="/signup">Sign up</NavLink>
+            <NavLink className="no-underline"to="/cart">
                 <div className="nav-cart">
                     <img src="https://www.svgrepo.com/show/80543/shopping-cart-outline.svg" alt="cart-icon" style={{width:'30px'},{height:'30px'}} />
+                    <div>{cart.totalQuantity()} item(s) </div>
                 </div>
             </NavLink>
         </div>
