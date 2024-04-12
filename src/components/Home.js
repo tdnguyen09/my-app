9a4577dcefd6eed06409b432e2e5a71b105aa282
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-04-12 16:59:00 +1000

    display some products in home componenets pictures

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,9 @@ function Home(){
     let clearanceToys = cart.allToyDatas.filter((toy) => toy.clearance === true);
     return (
         <div id="home">
-            <img src="https://as1.ftcdn.net/v2/jpg/04/76/79/70/1000_F_476797090_GD9J6JoeR956o2yXxMBEqQEw0xnlgAx5.jpg" alt="toy-background" />
+            <div className="pictures">
+            </div>
+            {/* <img src="https://as1.ftcdn.net/v2/jpg/04/76/79/70/1000_F_476797090_GD9J6JoeR956o2yXxMBEqQEw0xnlgAx5.jpg" alt="toy-background" /> */}
             <div className="home-products-container">
                 <div className="new-arrival">
                     <h2>NEW ARRIVAL</h2>
