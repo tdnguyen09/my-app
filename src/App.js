0c66ba2db209b3fefc3d245975a59dba1de3a2ee
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-04-14 01:07:05 +1000

    cleaning code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { useContext } from 'react';
+import React, from 'react';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import Login from './components/Login';
 import Logout from './components/Logout';
-import { Redirect, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import NavBar from './components/NavBar';
 import SignUp from './components/Signup';
 import ProductList from './components/ProductList';
 import ProductDetails from './components/ProductDetails';
 import Cart from './components/Cart';
-import { CartContext, CartProvider } from './context/CartContext';
+import { CartProvider } from './context/CartContext';
 
 
 function App() {
