bc481a1736b8f5466ee2c32041fe5d912c0b4a8d
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-04-13 20:30:49 +1000

    delete old image

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,6 @@ function Home(){
         <div id="home">
             <div className="pictures">
             </div>
-            {/* <img src="https://as1.ftcdn.net/v2/jpg/04/76/79/70/1000_F_476797090_GD9J6JoeR956o2yXxMBEqQEw0xnlgAx5.jpg" alt="toy-background" /> */}
             <div className="home-products-container">
                 <div className="new-arrival">
                     <h2>NEW ARRIVAL</h2>
